f51d4c36d302843e6883383f4a22579b6e2a7d49
ThomasCornillet <thomas.cornillet@gmail.com>
2022-07-29 15:02:01 +0200

    allegement listener

--- src/main/java/fr/eni/encheres/listeners/ListenerDemarrageFinVente.java
+++ src/main/java/fr/eni/encheres/listeners/ListenerDemarrageFinVente.java
@@ -25,36 +25,22 @@ import fr.eni.encheres.exceptions.BusinessException;
 public class ListenerDemarrageFinVente implements ServletContextListener {
 	private static Date dateDemarrage;
 
-	public static Date getDateDemarrage() {
-		return dateDemarrage;
-	}
-
 	/**
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
     public void contextInitialized(ServletContextEvent sce)  { 
-         // TODO checker ici si une vente est terminée et si oui, la traitée comme telle
-    	// on récupère les ventes en cours
     	ArticlesManager articlesMngr = ArticlesManager.getInstance();
     	List<Articles> listeArticles = new ArrayList<>();
     	try {
     		listeArticles = articlesMngr.selectAll();
-    		for (Articles a : listeArticles) { // TODO il doit y avoir moyen de réccourcir la liste
+    		for (Articles a : listeArticles) { // TODO il doit y avoir moyen de raccourcir la liste
         		if (a.getDate_fin_enchere().isEqual(LocalDate.now()) || a.getDate_fin_enchere().isBefore(LocalDate.now())) {
-            		// si la date de fin est aujourd'hui ou avant aujourdh'ui la vente est terminée
-        			// TODO voir ce qu'on fait si la date est passée mais que personne n'a enchérie
-        			// on check d'abord si la vente n'est pas déjà indiquée comme terminée // TODO à améliorer avec des attributs d'application
         			if (!a.isVendu()) {
-
-//        				System.out.println(a.toString());
         				// pas encore marqué comme vendu
         				// on effectue le paiement  
         				UtilisateursManager utilisateursMngr = UtilisateursManager.getInstance();
         				EncheresManager encheresMngr = EncheresManager.getInstance();
-//        				Encheres enchereGagnante = encheresMngr.selectEnchereGagnateByNoArticle(a.getNoArticle());
-//        				System.out.println(a.getNoArticle());
         				List<Encheres> listeEncheres = encheresMngr.selectByNoArticle(a.getNoArticle());
-//        				System.out.println(listeEncheres.toString());
         				Utilisateurs vendeur = utilisateursMngr.selectById(a.getNo_utilisateur());
         				if (!listeEncheres.isEmpty()) {
         					Encheres enchereGagnante = listeEncheres.get(0);
@@ -70,12 +56,10 @@ public class ListenerDemarrageFinVente implements ServletContextListener {
 	        						// faire une alerte, ou alors gérer le paiement dès l'enchère
 	        				}
         				} else {
-        					// TODO qu'est-ce qu'on fait si une vente est terminée est qu'il n'y a pas d'enchères ?
+        					a.setPrix_vente(0);
         				}
-        				// on la met en vendu dans la BDD
+        				a.setVendu(true);
         				articlesMngr.updateVenteTerminee(a);
-        				
-        				
         			}
         		}
         	}
